08fa82a2031e454701a0af3351465ceedf4d2a78
Beslin Ebby <beslinebby00@gmail.com>
2025-12-04 20:58:25 +0530

    added input section with store functionality to add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import TaskInput from "./components/TaskInput";
 
 const App = () => {
+   const [input, setInput] = useState("");
+   const [items, setItems] = useState([]);
+
+   const inputRef = useRef();
+
+   const storeTasks = () => {
+      setItems([...items, input]);
+      setInput("");
+   };
    return (
       <section className="container">
          <div className="head-sec">
             <h2>Mark The Tasks</h2>
+            <TaskInput input={input} setInput={setInput} inputRef={inputRef} storeTasks={storeTasks} />
          </div>
       </section>
    );
---
+++ src/components/TaskInput.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const TaskInput = ({inputRef, input, setInput, storeTasks}) => {
+  return (
+     <div className='input-sec'>
+        <input
+           ref={inputRef}
+           required
+           type="text"
+           placeholder="Enter items..."
+           value={input}
+           onChange={(e) => setInput(e.target.value)}
+        />
+        <button disabled={input.length === 0} onClick={storeTasks}>
+           Add
+        </button>
+     </div>
+  );
+}
+
+export default TaskInput
